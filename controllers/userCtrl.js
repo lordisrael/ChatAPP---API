10cbffc4879a9233ce2d1f6de09c14d9824585df
lordisrael <josephisrael206@gmail.com>
2023-12-11 19:54:27 +0100

    Updating necessary functionalities

---
+++ controllers/userCtrl.js
@@ -0,0 +1,26 @@
+const User = require('../models/user')
+const asyncHandler = require("express-async-handler");
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthenticatedError,
+} = require("../errors");
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
+
+
+const createUser = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const userAlreadyExist = await User.findOne({ email });
+  if (!userAlreadyExist) {
+    const user = await User.create(req.body);
+    const token = createJWT(user._id, user.firstname);
+    res.status(StatusCodes.CREATED).json({ user, token: token });
+  } else {
+    throw new BadRequestError("Email already exist");
+  }
+});
+
+module.exports = {
+    createUser
+}
--- models/user.js
+++ models/user.js
@@ -1,68 +1,87 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 // Define User Schema
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePicture: {
-    type: String,
-    default: "default_profile_picture.jpg", // Default profile picture URL
-  },
-  bio: {
-    type: String,
-    default: "",
-  },
-  // Array to store friends or connections
-  friends: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-  // Additional fields
-//   status: {
-//     type: String,
-//     enum: ["Online", "Offline", "Away"],
-//     default: "Offline",
-//   },
-  lastSeen: {
-    type: Date,
-    default: Date.now,
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide valid email",
+      ],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "default_profile_picture.jpg", // Default profile picture URL
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    // Array to store friends or connections
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    // Additional fields
+    //   status: {
+    //     type: String,
+    //     enum: ["Online", "Offline", "Away"],
+    //     default: "Offline",
+    //   },
+    lastSeen: {
+      type: Date,
+      default: Date.now,
+    },
+    // Array to store friend requests
+    friendRequests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    // Array to store blocked users
+    blockedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    // Array to store chat messages or conversations
+    conversation: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chat",
+      },
+    ],
+
+    refreshToken: {
+      type: String,
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
-  // Array to store friend requests
-  friendRequests: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  // Array to store blocked users
-  blockedUsers: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  // Array to store chat messages or conversations
-  conversation: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Chat",
-    },
-  ],
-});
+  {
+    timestamps: true,
+  }
+);
 
 // Virtual field for confirmPassword
 userSchema.virtual('confirmPassword')
@@ -104,6 +123,19 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
     return false;
   }
 };
+
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  console.log(resetToken, this.passwordResetToken);
+  return resetToken;
+};
+
 
 // Create User model using the schema
 const User = mongoose.model("User", userSchema);
