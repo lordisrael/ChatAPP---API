bb2fe1bb8bf1b02ea44a796c4d9f4116e83b7d0a
lordisrael <josephisrael206@gmail.com>
2023-12-17 07:11:59 +0100

    Refactor the code

--- controllers/chatCtrl.js
+++ controllers/chatCtrl.js
@@ -1,72 +1,10 @@
 const Chat = require("../models/chat");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-// const app = require("express")();
-// const http = require("http").Server(app);
-// const io = require("socket.io")(http);
-// const mongoose = require("mongoose");
-
-// Assuming you have defined the Conversation model and set up other configurations
-
-
-// Endpoint for creating a conversation between two users or sending subsequent messages
-// app.post("/conversations", async (req, res) => {
-//   try {
-//     const receiver = req.params
-//     const message = req.body;
-//     const sender = req.user._id;
-
-//     // Check if a conversation already exists between the sender and receiver
-//     const existingConversation = await Chat.findOne({
-//       participants: { $all: [sender, receiver] },
-//     });
-
-//     let conversation;
-
-//     if (existingConversation) {
-//       // If conversation exists, add a new message to the existing conversation
-//       existingConversation.messages.push({
-//         sender,
-//         text: message,
-//       });
-//       conversation = await existingConversation.save();
-//     } else {
-//       // If no conversation exists, create a new conversation
-//       const newConversation = new Conversation({
-//         participants: [sender, receiver],
-//         messages: [
-//           {
-//             sender,
-//             text: message,
-//           },
-//         ],
-//       });
-//       conversation = await newConversation.save();
-//     }
-
-//     // Emit an event to inform both participants about the new message
-//     const participants = [sender, receiver];
-//     participants.forEach((participantId) => {
-//       io.to(participantId).emit("newMessage", {
-//         conversationId: conversation._id,
-//         message,
-//       });
-//     });
-
-//     res.status(201).json({ conversation });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-
 // Function to handle sending messages
 const sendMessage = async (socket, senderId, receiverId, text) => {
   try {
-    //const { senderId, receiverId, text } = data;
     // const { text } = data;
-    // const senderId = socket.user._id;
-    // const receiverId = socket.handshake.query.receiverId;
 
     if (!text || text.trim() === "") {
       console.error("Empty message text");
@@ -74,9 +12,12 @@ const sendMessage = async (socket, senderId, receiverId, text) => {
     }
 
     // Check if a chat exists between these two users
-    let chat = await Chat.findOne({
-      participants: { $all: [senderId, receiverId] },
-    });
+     let chat = await Chat.findOne({
+       $or: [
+         { participants: { $all: [senderId, receiverId] } },
+         { participants: { $all: [receiverId, senderId] } }, // Check both combinations
+       ],
+     });
 
     // If no chat exists, create a new one
     if (!chat) {
@@ -108,9 +49,7 @@ const sendMessage = async (socket, senderId, receiverId, text) => {
 const verifyToken = (token) => {
   try {
     // Verify the token and decode the payload
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Replace 'your_secret_key' with your actual secret key
-    // const user = User.findById(decoded.id);
-    // socket.user = user;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); 
     return decoded;
   } catch (error) {
     return null; // If token verification fails, return null
@@ -148,7 +87,6 @@ const handleChatEvents = (io) => {
   };
 
   // Apply the authentication middleware to your socket connection
-  //io.use(authenticateSocket);
   io.use(authenticateSocket, (error) => {
     console.error(error.message);
     socket.disconnect();
@@ -158,48 +96,6 @@ const handleChatEvents = (io) => {
     console.log(`Socket connected: ${socket.id}`);
 
     const receiverId = socket.handshake.query.receiverId;
-
-    // Function to verify the JWT token and retrieve user information
-
-    // const authenticateSocket = async (socket, next) => {
-    //   // const token =
-    //   //   socket.handshake.Headers.Authorization ||
-    //   //   socket.handshake.query.token ||
-    //   //   socket.request.headers.token ||
-    //   //   socket.handshake.Headers.token ||
-    //   //   socket.handshake.headers.access_token;;
-    //   const token = socket.request.headers.token;
-    //   console.log("Received token:", token);
-    //   //socket.request.cookies.my_token;
-    //   const payLoad = await verifyToken(token);
-    //   if (!payLoad) {
-    //     console.error("Invalid token");
-    //     next(new Error("Invalid token"));
-    //     return;
-    //   }
-
-    //   const user = await User.findById(payLoad.id);
-    //   if (!user) {
-    //     console.error("User not found");
-    //     next(new Error("User not found"));
-    //     return;
-    //   }
-
-    //   socket.user = user;
-    //   next();
-    // };
-
-    // // Apply the authentication middleware to your socket connection
-    // //io.use(authenticateSocket);
-    // io.use(authenticateSocket, (error) => {
-    //   console.error(error.message);
-    //   socket.disconnect();
-    // });
-
-    // // Handle sending messages
-    // // socket.on("message", (data) => {
-    // //   sendMessage(socket, data);
-    // // });
 
     // Handle sending messages
     socket.on("message", async (text) => {
@@ -227,16 +123,11 @@ const handleChatEvents = (io) => {
         return;
       }
 
-      // const senderId = socket.user._id; // Fetch senderId from authenticated user via socket
-
-      // // Call sendMessage function with senderId, receiverId, and text
-      // sendMessage(io, senderId, receiverId, text);
     });
 
     // Handle disconnection
     socket.on("disconnect", () => {
       console.log(`Socket disconnected: ${socket.id}`);
-      // Additional cleanup or handling can be added here if needed
     });
   });
 };
