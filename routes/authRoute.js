480f87cb5dfecce27c870893744281f6f31e2a55
lordisrael <josephisrael206@gmail.com>
2023-12-19 23:49:40 +0100

    added upload profile picture routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,13 +4,25 @@ const router = express.Router();
 const {
   createUser,
   login,
+  uploadProfilePicture,
   
 } = require("../controllers/userCtrl");
 
 const auth = require("../middleware/authMiddleware");
+const {
+  uploadPhoto,
+  userImgResize,
+} = require("../middleware/uploadImageMiddleware");
 
 router.post("/register", createUser);
 router.post("/login", login);
+router.put(
+  "/upload",
+  auth,
+  uploadPhoto.single("image"),
+  userImgResize,
+  uploadProfilePicture
+);
 
 
 module.exports = router;
