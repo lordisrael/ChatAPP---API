01b2f214f0991e2b80e5f3639060f448458fa85c
lordisrael <josephisrael206@gmail.com>
2023-12-19 23:48:06 +0100

    added upload profilepicture endpoints

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -5,10 +5,16 @@ const {
   BadRequestError,
   UnauthenticatedError,
 } = require("../errors/index");
+const cloudinaryUploadImg = require("../utils/cloudinary");
 const { createJWT } = require("../config/jwt");
 const { createRefreshJWT } = require("../config/refreshToken");
+const {
+  uploadPhoto,
+  productImgResize,
+} = require("../middleware/uploadImageMiddleware");
 const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
+const fs = require('fs')
 
 
 const createUser = asyncHandler(async (req, res) => {
@@ -58,8 +64,45 @@ const login = asyncHandler(async (req, res) => {
   }
 });
 
+  const uploadProfilePicture = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    //console.log(req.file)
+    const uploader = (path) => cloudinaryUploadImg(path, "image");
+    let imageUrl = "";
+    
+    const { path: filePath } = req.file;
+    const newpath = await uploader(filePath);
+    console.log(newpath);
+    imageUrl = newpath;
+    //fs.unlinkSync(newpath);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      { _id },
+      {
+        profilePicture: imageUrl.url, // Assuming 'image' is the field in your User model for the profile picture
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedUser) {
+      throw new NotFoundError(`No user found`);
+    }
+    res
+      .status(StatusCodes.OK)
+      .json({ updatedUser, msg: "Profile picture updated" });
+  });
+const deleteProfilePicture = asyncHandler(async(req, res) => {
+
+})
+
+const editBio = asyncHandler(async(req, res) => {
+
+})
 
 module.exports = {
     createUser,
-    login
+    login,
+    uploadProfilePicture
 }
