3500b73f911e4409e09e760c9f7d7bafc004bfe6
lordisrael <josephisrael206@gmail.com>
2024-01-01 20:58:48 +0100

    Create Group model for user

--- models/group.js
+++ models/group.js
@@ -1,21 +1,49 @@
 const mongoose = require("mongoose");
 
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  messageType: {
+    type: String,
+    enum: ["text", "image", "video"],
+  },
+  text: String,
+  imageUrl: [String],
+  videoUrl: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
 // Group Schema
 const groupSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-  isPrivate: {
-    type: Boolean,
-    default: false,
+  groupProfilePicture: {
+    type: String,
+    default: process.env.DEFAULT_PROFILE_PICTURE_URL,
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  admin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
   members: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User", // Reference to a User model (assuming you have a User model)
+      ref: "User",
     },
   ],
+  messages: [messageSchema],
 });
 
 // Group model
