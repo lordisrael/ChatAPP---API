11dd363dbd5949f3b179700e8f193f7cd3867e20
lordisrael <josephisrael206@gmail.com>
2024-01-03 14:22:32 +0100

    Added group-bio and deleting group functionalities

--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {createGroup} = require('../controllers/grpMsgCtrl')
+const {createGroup, deleteGroup, groupBio} = require('../controllers/grpMsgCtrl')
 
 const auth = require("../middleware/authMiddleware");
 
 router.post("/create", auth, createGroup);
+router.put("/edit-bio/:groupId", auth, groupBio)
+router.delete("/delete/:groupId", auth, deleteGroup)
+
 
 module.exports = router;
