ad79006d7639fe5b698ff07261cfb0227ecf6043
lordisrael <josephisrael206@gmail.com>
2024-01-04 20:56:37 +0100

    Added route to delete message by sender

--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {createGroup, deleteGroup, groupBio} = require('../controllers/grpMsgCtrl')
+const {createGroup, deleteGroup, groupBio, addFriendstoGrp, deleteMsgBySender} = require('../controllers/grpMsgCtrl')
 
 const auth = require("../middleware/authMiddleware");
 
+router.post("/:groupId/users/:userId", auth, addFriendstoGrp);
+router.delete("/:groupId/message/:messageId", auth, deleteMsgBySender);
 router.post("/create", auth, createGroup);
 router.put("/edit-bio/:groupId", auth, groupBio)
 router.delete("/delete/:groupId", auth, deleteGroup)
