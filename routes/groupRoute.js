53f135f32ab4aec08b33cbd43ea5aa73a3e6440c
lordisrael <josephisrael206@gmail.com>
2024-01-06 06:42:33 +0100

    Route for user to leave group

--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const {createGroup, deleteGroup, groupBio, addFriendstoGrp, deleteMsgBySender} = require('../controllers/grpMsgCtrl')
+const {createGroup, deleteGroup, groupBio, addFriendstoGrp, deleteMsgBySender, sendPICorVID} = require('../controllers/grpMsgCtrl')
+const {
+  uploadPhotoOrVideo,
+  chatImgResize,
+  chatMediaResize,
+} = require("../middleware/uploadImageOrVideoMiddleware");
 
 const auth = require("../middleware/authMiddleware");
+
 
 router.post("/:groupId/users/:userId", auth, addFriendstoGrp);
 router.delete("/:groupId/message/:messageId", auth, deleteMsgBySender);
 router.post("/create", auth, createGroup);
 router.put("/edit-bio/:groupId", auth, groupBio)
 router.delete("/delete/:groupId", auth, deleteGroup)
+
+router.put(
+  "/message/upload/:id",
+  auth,
+  uploadPhotoOrVideo.array("media", 10),
+  chatImgResize,
+  chatMediaResize,
+  sendPICorVID
+);
 
 
 module.exports = router;
