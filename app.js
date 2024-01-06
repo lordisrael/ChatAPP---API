03999189f556926c49fdb0d3597f667a170d6de5
lordisrael <josephisrael206@gmail.com>
2024-01-06 16:15:17 +0100

    Implemented cors, helmmet, swagger packages

--- app.js
+++ app.js
@@ -3,12 +3,33 @@ require("express-async-errors");
 const express = require("express");
 const http = require("http");
 
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+
+//swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 
 const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+
+// app.set("trust proxy", 1);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000,
+//     max: 100,
+//   })
+// );
+app.use(helmet.xssFilter());
+// app.use(cors());
+// app.use(xss());
 
 const socketIO = require("socket.io");
 
@@ -23,6 +44,11 @@ const searchRoute = require('./routes/searchRoutes')
 
 app.use(express.json());
 app.use(cookieParser());
+
+// app.get("/", (req, res) => {
+//   res.send('<h1>E commerce</h1><a href="/api-docs">Documentation</a>');
+// });
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/auth", authRoute);
 app.use("/api/chat", chatRoute)
